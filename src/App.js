9494ec16159b43fb9ba46a337cc0d1d7f198935f
daniel-bergmann <dba222@outlook.com>
2022-04-16 16:07:45 +0000

    made a lyrics finding app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [artist, setArtist] = useState("Lady Gaga");
+  const [song, setSong] = useState("Pokerface");
+
+  useEffect(() => {
+    const getLyrics = async () => {
+      const lyricsFromServer = await getApiData();
+      setData(lyricsFromServer);
+    };
+    getLyrics();
+  }, [artist, song]);
+
+  // fetch function
+  const getApiData = async () => {
+    const res = await fetch(`${process.env.REACT_APP_API}${artist}/${song}`);
+    const apiData = await res.json();
+    return apiData;
+  };
+
+  // Onchange handlers for the form
+  const lookForSongHandleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(artist, song);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <form onSubmit={lookForSongHandleSubmit}>
+        <input
+          id="artist"
+          placeholder={artist}
+          name="artist"
+          type="text"
+          value={artist}
+          onChange={(e) => setArtist(e.target.value)}
+        />
+        <input
+          id="song"
+          placeholder={song}
+          name="song"
+          type="text"
+          value={song}
+          onChange={(e) => setSong(e.target.value)}
+        />
+        <button type="submit">submit</button>
+      </form>
+      {data.lyrics}
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
