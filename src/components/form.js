a80c4c2bc806ef693a90ca4b10008d1f0b364a1b
daniel-bergmann <dba222@outlook.com>
2022-04-19 21:34:37 +0000

    fixed width for mobile view and added more whitespace

--- src/components/form.js
+++ src/components/form.js
@@ -18,6 +18,7 @@ export default function Form({
         value={artist}
         onChange={(e) => setArtist(e.target.value)}
       />
+      {/* this will appear if the fields are empty */}
       {submitted && !artist ? <span>Please input artist name.</span> : null}
       <input
         id="song"
@@ -28,7 +29,10 @@ export default function Form({
         onChange={(e) => setSong(e.target.value)}
       />
       {submitted && !song ? <span>Please input song name.</span> : null}
-      {/* <button type="submit">submit</button> */}
+      {/* The button will init error handling if input is empty */}
+      <button type="submit">
+        <strong>Go</strong>
+      </button>
     </Container>
   );
 }
@@ -38,11 +42,11 @@ const Container = styled.form`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 20vh;
+  height: 30vh;
 
   input {
-    padding: 10px 40px;
-    margin: 10px;
+    padding: 5px 0;
+    margin: 10px 0;
     border: none;
     border-bottom: 2px solid tomato;
     background-color: #222222;
@@ -52,5 +56,34 @@ const Container = styled.form`
   input:focus {
     outline: none;
     color: tomato;
+  }
+
+  button,
+  input[type="submit"],
+  input[type="reset"] {
+    background: tomato;
+    color: #333;
+    border: 2px solid rgb(209, 209, 209);
+    padding: 10px;
+    margin: 20px;
+    cursor: pointer;
+    width: 100%;
+    font-size: 1rem;
+  }
+
+  /* Larger screens */
+  @media (min-width: 768px) {
+    height: 40vh;
+    button,
+    input[type="submit"],
+    input[type="reset"] {
+      padding: 10px;
+      width: 10%;
+      font-size: 1rem;
+    }
+
+    button:hover {
+      border: 2px solid #222222;
+    }
   }
 `;
